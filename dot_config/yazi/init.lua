b9e83e0551b55ba84ed0011e93b1ebe954111c5a
FPA-NathanHuynh <nhanhuynh296@gmail.com>
2025-05-09 10:50:24 +1200

    Added yazi and oh-my-zsh

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,22 @@
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+}
+
+require("git"):setup()
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+return {
+	entry = function()
+		local h = cx.active.current.hovered
+		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+	end,
+}
---
+++ run_repeatable.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+ya pkg install
