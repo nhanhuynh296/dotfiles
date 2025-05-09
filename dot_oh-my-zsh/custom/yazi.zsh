eb77b7b355b9f43476d9d6b6b65c3ab28ab7886f
Nathan Huynh <nhanhuynh296@gmail.com>
2025-05-10 10:13:08 +1200

    Using chezmoi cli to add

---
+++ dot_oh-my-zsh/custom/yazi.zsh
@@ -0,0 +1,9 @@
+# ability to change the current working directory when exiting Yazi
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
